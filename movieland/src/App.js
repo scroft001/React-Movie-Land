040e30e95297be934b865d40e1b9ead668fd2a60
Steven Croft <scroft001@gmail.com>
2022-09-27 22:22:51 -0700

    Placeholder setup

--- movieland/src/App.js
+++ movieland/src/App.js
@@ -1,22 +1,54 @@
 import React from 'react';
 import { useEffect } from 'react';
+import './App.css';
+import SearchIcon from './search.svg';
 
 const API_URL = 'http://www.omdbapi.com?apikey=4ff64b39';
+
+const movie1 = {
+    "Title": "Amazing Spiderman Syndrome",
+    "Year": "2015",
+    "imdbID": "tt2586634",
+    "Type": "movie",
+    "Poster": "N/A"
+}
 
 const App = () => {
 
     const searchMovies = async (title) => {
         const response = await fetch(`${API_URL}&s=${title}`);
         const data = await response.json();
-        console.log(data);
+        console.log(data.search);
     }
 
     useEffect(() => {
-        searchMovies("Thor")
+        searchMovies("Thor");
     }, []);
 
     return (
-        <h1>Movies</h1>
+        <div className='app'>
+            <h1>MovieLand</h1>
+
+            <div className='search'>
+                <input placeholder='Search for movies' value='superman' onChange={() => {}}/>
+                <img src={SearchIcon} alt='search' onClick={() => {}}/>
+            </div>
+
+            <div className='container'>
+                <div className='movie'>
+                    <div>
+                        <p>{movie1.Year}</p>
+                    </div>
+                    <div>
+                        <img src={movie1.Poster !== 'N/A' ? movie1.Poster : 'https://via.placeholder.com/404'}/>
+                    </div>
+                    <div>
+                        <span>{movie1.Type}</span>
+                        <h3>{movie1.Title}</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
