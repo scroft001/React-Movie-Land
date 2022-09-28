2fb871739addb8968e2b3ad294f873f463d789a1
Steven Croft <scroft001@gmail.com>
2022-09-27 22:05:13 -0700

    API request

--- movieland/src/App.js
+++ movieland/src/App.js
@@ -1,6 +1,20 @@
 import React from 'react';
+import { useEffect } from 'react';
+
+const API_URL = 'http://www.omdbapi.com?apikey=4ff64b39';
 
 const App = () => {
+
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+        console.log(data);
+    }
+
+    useEffect(() => {
+        searchMovies("Thor")
+    }, []);
+
     return (
         <h1>Movies</h1>
     )
