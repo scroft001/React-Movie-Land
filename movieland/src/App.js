d742ff54261b245e73d96ca8b1972e1f0f5a6cba
Steven Croft <scroft001@gmail.com>
2022-09-28 22:02:25 -0700

    Dynamic searches

--- movieland/src/App.js
+++ movieland/src/App.js
@@ -6,17 +6,10 @@ import MovieCard from './MovieCard';
 
 const API_URL = 'http://www.omdbapi.com?apikey=4ff64b39';
 
-const movie1 = {
-    "Title": "Amazing Spiderman Syndrome",
-    "Year": "2015",
-    "imdbID": "tt2586634",
-    "Type": "movie",
-    "Poster": "N/A"
-}
-
 const App = () => {
 
     const [movies, setMovies] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
 
     const searchMovies = async (title) => {
         const response = await fetch(`${API_URL}&s=${title}`);
@@ -33,12 +26,12 @@ const App = () => {
             <h1>MovieLand</h1>
 
             <div className='search'>
-                <input placeholder='Search for movies' value='superman' onChange={() => {}}/>
-                <img src={SearchIcon} alt='search' onClick={() => {}}/>
+                <input placeholder='Search for movies' value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)}/>
+                <img src={SearchIcon} alt='search' onClick={() => {searchMovies(searchTerm)}}/>
             </div>
 
             {
-                movies ?.length > 0 ?(
+            movies ?.length > 0 ?(
                     <div className='container'>
                         {
                             movies.map((movie) => <MovieCard movie={movie}/>)
